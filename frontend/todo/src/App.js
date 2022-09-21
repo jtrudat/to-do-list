058e8b083a1fd38b0ed8433d936ee9ad6f9e0357
jeff truitt <jtruitt@gmail.com>
2022-09-21 01:56:24 -0700

    touchups and descriptions

--- frontend/todo/src/App.js
+++ frontend/todo/src/App.js
@@ -1,37 +1,44 @@
 import './App.css'
 import { Input } from './components/Input';
 import { ListItems } from './components/ListItems'
+import { FaCarSide, FaRunning } from 'react-icons/fa'
 import { useState } from 'react'
 
+
+//FOR DEMONSTRATION PURPOSES ONLY, WHEN INTERNET ACCESS TO THE DATABASE IS UNAVAILABLE
+//DUMMYDATA IS PASSED AS A PROPERTY FROM THE APP COMPONENT TO THE LISTITEMS COMPONENTS 
+//TO BE USED AS AN INITIAL STATE SO THAT A PRACTICE DATA SOURCE CAN BE PRESENT
 let dummyData = [
   {
     _id: '1',
     todoItem: 'paint the fence',
-    date: '2022-09-15'
+    date: '2022-09-15',
+    key: 11
   },
   {
     _id: '2',
     todoItem: 'walk the dog',
-    date: '2022-09-14'
+    date: '2022-09-14',
+    key: 21
   },
   {
     _id: '3',
     todoItem: 'water the flowers',
-    date: '2022-09-13'
+    date: '2022-09-13',
+    key: 31
   }
 ]
 
 function App() {
-  const [ itemsList, setItemsList ] = useState(dummyData)
+  let itemsList = useState(dummyData)
+  
 
-  // const addNewItemHandler = (newestItem)=>{
-  //   setItemsList((itemsList)=>{
-  //     return[newestItem, ...itemsList]
-  //   })
-  // }
+
+//THE MAIN APP COMPONENT HAS TWO SUBCOMPONENTS, THE INPUT AND THE LISTITEMS. 
+//LISTITEMS IS PASSED THE TASKERS PROPERTY WHICH CONTAINS THE INTERNALLY STORED DUMMY DATA.  
   return (
     <div className="setDisplay">
-      <div><h3><b>Event items for vehicle care</b></h3></div>
+      <div><h3><b><FaCarSide className="sidecar"/>- Taskers & To-do's quicksheet - <FaRunning className="battery"/></b></h3></div>
       <Input />
       <ListItems taskers={itemsList}/>
     </div>
