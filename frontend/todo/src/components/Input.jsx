a4f2b03cf3431cc4b124a456a314102226af8559
jeff truitt <jtruitt@gmail.com>
2022-09-23 15:22:47 -0700

    input component updates

--- frontend/todo/src/components/Input.jsx
+++ frontend/todo/src/components/Input.jsx
@@ -3,47 +3,46 @@ import axios from 'axios'
 import './Input.css'
 
 export const Input = (props)=>{
-    let [itemName, setItemName] = useState('')
-    let [enteredDate, setEnteredDate] = useState('')
+    let [item, setItem] = useState('')
+    let [date, setDate] = useState('')
 
     //ASSIGNS THE NEWLY INPUT DATA TO AN OBJECT. 
     //THE OBJECT IS THEN ROUTED INTO THE ITEM MODEL ON THE SERVER AND THEN POSTED TO THE COLLECTIONS MODEL
-    let submitHandler = (evt)=>{
+    let handleSubmit = (evt)=>{
         evt.preventDefault()
         let newItem = {
-            todoItem : itemName,
-            date : enteredDate
+            todoItem : item,
+            date : date
         }
         axios.post('/todos', newItem)
-        setItemName('')
-        setEnteredDate('')
-        //window.location.reload(true)
+        setItem('')
+        setDate('')
         props.onRefresh()
         }   
     
     //SETS THE STATE FOR THE DESIRED ITEM TASKER TO BE ENTERED INTO THE DATABASE
-    let itemChangeHandler = (evt)=>{
-        setItemName(evt.target.value)
+    let handleItemChange = (evt)=>{
+        setItem(evt.target.value)
         console.log(evt.target.value)
     }
 
     //SETS THE STATE FOR THE DESIRED ITEM TASKER DATE TO BE ENTERED INTO THE DATABASE
-    let dateChangeHandler = (evt)=>{
-        setEnteredDate(evt.target.value)
+    let handleDateChange = (evt)=>{
+        setDate(evt.target.value)
         console.log(evt.target.value)
     }
 
     return(
-        <form onSubmit={submitHandler}>
+        <form onSubmit={handleSubmit}>
         <div>
             <div>
             <label>To Do Item 👉 </label>
-            <input className="inner" type="text" maxLength="19" size="24" value={itemName} placeHolder="list your item here" onChange={itemChangeHandler}></input>
+            <input className="inner" type="text" maxLength="19" size="24" value={item} placeHolder="list your item here" onChange={handleItemChange}></input>
             </div>
             <br></br>
             <div>
                 <label>Due by 👉 </label>
-                <input className="inputdate" type="datetime-local" min='2022-09-01' max='2023-01-01' value={enteredDate} onChange={dateChangeHandler}></input>
+                <input className="inputdate" type="datetime-local" min='2022-09-01' max='2023-01-01' value={date} onChange={handleDateChange}></input>
             <button className="submitter" type="submit">Add 📝</button>
             </div>   
         </div>
