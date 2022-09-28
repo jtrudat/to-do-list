2e92bcb78d49b202e6d8da501e5cdb635ea7b32a
jeff truitt <jtruitt@gmail.com>
2022-09-28 10:47:13 -0700

    updates

--- frontend/todo/src/components/Input.jsx
+++ frontend/todo/src/components/Input.jsx
@@ -36,8 +36,8 @@ export const Input = (props)=>{
         <form>
         <div>
             <div>
-            <label>Task Item : </label>
-            <input className="inner" type="text" maxLength="31" size="28" value={item} placeholder="list your item here" onChange={handleItemChange}></input>
+            <label>Task 👉 </label>
+            <input className="inner" type="text" maxLength="31" size="28" value={item} placeholder="Enter your item here" onChange={handleItemChange}></input>
             </div>
             <br></br>
             <div>
